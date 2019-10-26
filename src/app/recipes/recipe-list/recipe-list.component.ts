8c8eac1c148bb93a826fbd18d61c98421bbfa3e1
kikiBg <kristinarsic@gmail.com>
2019-10-26 20:35:11 +0200

    header and recipe component fill part1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new  Recipe('Test recipe', 'This is a test', 'http://www.donesi.com/blog/wp-content/uploads/2014/11/palacinke-1024x685.jpg'),
+    new  Recipe('Test recipe', 'This is a test', 'http://www.donesi.com/blog/wp-content/uploads/2014/11/palacinke-1024x685.jpg')
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
