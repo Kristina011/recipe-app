d2300db4dad125b69f4cc7a5e3c44f4e2e2fcfbc
kikiBg <kristinarsic@gmail.com>
2020-01-02 15:24:25 +0100

    forms - reactive approach

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs';
 
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
@@ -6,6 +7,7 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipeChanged = new Subject;
 
   private recipes: Recipe[] = [
     new  Recipe(
@@ -36,12 +38,28 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  getRecipe(index: number){
+  getRecipe(index: number) {
       return this.recipes[index];
   }
 
   addIngredientsToSl(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+      this.recipes.splice(index, 1);
+      this.recipeChanged.next(this.recipes.slice());
+  }
+
 }
 
